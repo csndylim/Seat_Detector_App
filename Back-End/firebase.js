ea19a9d2b8a7dce09e6ed981758d84de1b4c74fc
csndylim <54385174+csndylim@users.noreply.github.com>
2021-10-05 17:22:06 +0800

    Revert "change firebase api"
    This reverts commit 4775f5a7e59abd5b571ca19b670abb96b281a0ec.

--- Back-End/firebase.js
+++ Back-End/firebase.js
@@ -2,13 +2,13 @@ const firebase = require('firebase');
 
 const db  = firebase.initializeApp(
     {
-        apiKey: "AIzaSyDBhrO6JEbVl8fd2s-zovBMM18ZbPsGQZo",
-        authDomain: "anything-b35f8.firebaseapp.com",
-        projectId: "anything-b35f8",
-        storageBucket: "anything-b35f8.appspot.com",
-        messagingSenderId: "415478575296",
-        appId: "1:415478575296:web:484aa80b63797a8eeafd25",
-        measurementId: "G-MS685E56T5"
+        apiKey: "AIzaSyCSpEdAkTWd2HMTcY-cTeppis3K963HJH0",
+        authDomain: "smartlib-66322.firebaseapp.com",
+        projectId: "smartlib-66322",
+        storageBucket: "smartlib-66322.appspot.com",
+        messagingSenderId: "469013252936",
+        appId: "1:469013252936:web:346ee2436f298ae191417d",
+        measurementId: "G-LGSWT5K4MV"
     }
 )
 
