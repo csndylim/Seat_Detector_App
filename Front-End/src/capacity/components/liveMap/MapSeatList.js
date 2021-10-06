679b09f8658e1262861498289d9cc87bd3374217
Lee Ming De <leemingde@gmail.com>
2021-10-06 13:19:41 +0800

    Resize image

--- Front-End/src/capacity/components/liveMap/MapSeatList.js
+++ Front-End/src/capacity/components/liveMap/MapSeatList.js
@@ -3,6 +3,9 @@ import MapSeat from "./MapSeat";
 
 import './MapSeatList.css';
 
+const svgViewWidth = 1000;
+const svgViewHeight = 500;
+
 const DUMMY_DATA = [
     {
     cameraId: "CamA",
@@ -37,13 +40,13 @@ const MapSeatList = () => {
 
     return (
         <div className="svg-container">
-            <svg viewBox='0 0 1200 600'>
-                <text x="300" y="30" fill="black" className="section-header" textAnchor="middle">Section A</text>
-                <text x="900" y="30" fill="black" className="section-header" textAnchor="middle">Section B</text>
-                <text x="300" y="330" fill="black" className="section-header" textAnchor="middle">Section C</text>
-                <text x="900" y="330" fill="black" className="section-header" textAnchor="middle">Section D</text>
-                <line x1="600" y1="0" x2="600" y2="600" style={{stroke:"rgb(0,0,0)", strokeWidth: 3}} />
-                <line x1="0" y1="300" x2="1200" y2="300" style={{stroke:"rgb(0,0,0)", strokeWidth: 3}} />
+            <svg viewBox={`0 0 ${svgViewWidth} ${svgViewHeight}`}>
+                <text x={`${svgViewWidth/4}`} y={`${svgViewHeight/20}`}  fill="black" className="section-header" textAnchor="middle">Section A</text>
+                <text x={`${svgViewWidth/4 * 3}`} y={`${svgViewHeight/20}`} fill="black" className="section-header" textAnchor="middle">Section B</text>
+                <text x={`${svgViewWidth/4}`} y={`${svgViewHeight/20 *12}`} fill="black" className="section-header" textAnchor="middle">Section C</text>
+                <text x={`${svgViewWidth/4 * 3}`} y={`${svgViewHeight/20 * 12}`} fill="black" className="section-header" textAnchor="middle">Section D</text>
+                <line x1={`${svgViewWidth/2}`} y1="0" x2={`${svgViewWidth/2}`} y2={`${svgViewHeight}`} style={{stroke:"rgb(0,0,0)", strokeWidth: 3}} />
+                <line x1="0" y1={`${svgViewHeight/2}`}  x2={`${svgViewWidth}`}  y2={`${svgViewHeight/2}`}  style={{stroke:"rgb(0,0,0)", strokeWidth: 3}} />
                 {renderSeats}
             </svg>
         </div>
